c79d2623b86ee8cd96ae131a0198857622f2236f
skanda <skandavyas20@gmail.com>
2026-06-06 15:42:40 -0700

    Move download cue under render panel

--- app.py
+++ app.py
@@ -545,16 +545,18 @@ with right_col:
                     except Exception as exc:
                         st.error(f"Could not tune this file: {exc}")
 
+    if st.session_state.get("fixed_file_path"):
+        st.markdown(
+            """
+            <a class="download-jump" href="#download-export">
+                <span class="download-jump-text">Scroll down</span>
+                <span class="download-jump-chevron">⌄</span>
+            </a>
+            """,
+            unsafe_allow_html=True,
+        )
+
 if st.session_state.get("fixed_file_path"):
-    st.markdown(
-        """
-        <a class="download-jump" href="#download-export">
-            <span class="download-jump-text">Scroll down</span>
-            <span class="download-jump-chevron">⌄</span>
-        </a>
-        """,
-        unsafe_allow_html=True,
-    )
     st.divider()
     st.markdown('<div id="download-export" class="download-anchor"></div>', unsafe_allow_html=True)
     with st.container(border=True):
